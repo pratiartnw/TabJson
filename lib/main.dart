817c1d20ae7b7607eea3d952bbfe2e0e853485df
pratiaretnowidiawati16 <pratiaretnowidiawati16@gmail.com>
2024-08-08 03:43:38 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'tab_screen.dart';
+void main() {
+runApp(
+const MaterialApp(
+home: TabScreen(),
+),
+);
+}
---
+++ lib/tab_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+class TabScreen extends StatelessWidget {
+  const TabScreen({super.key});
+
+@override
+Widget build(BuildContext context) {
+return DefaultTabController(
+length: 2, // Number of tabs
+child: Scaffold(
+appBar: AppBar(
+title: const Text('Tab Menu'),
+bottom: const TabBar(
+tabs: [
+Tab(icon: Icon(Icons.person), text: 'Users'),
+Tab(icon: Icon(Icons.info), text: 'About'),
+],
+),
+),
+body: const TabBarView(
+children: [
+UsersTab(),
+AboutTab(),
+],
+),
+),
+);
+}
+}
+class UsersTab extends StatelessWidget {
+  const UsersTab({super.key});
+
+Future<List<User>> fetchUsers() async {
+final response =
+await http.get(Uri.parse('https://reqres.in/api/users?page=2'));
+if (response.statusCode == 200) {
+final List<dynamic> data = json.decode(response.body)['data'];
+return data.map((user) => User.fromJson(user)).toList();
+} else {
+throw Exception('Failed to load users');
+}
+}
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(
+title: const Text('User List'),
+),
+body: FutureBuilder<List<User>>(
+future: fetchUsers(),
+builder: (context, snapshot) {
+final users = snapshot.data!;
+return ListView.builder(
+itemCount: users.length,
+itemBuilder: (context, index) {
+final user = users[index];
+return ListTile(
+title: Text(user.firstName),
+subtitle: Text(user.email),
+);
+},
+);
+},
+),
+);
+}
+}
+class AboutTab extends StatelessWidget {
+  const AboutTab({super.key});
+
+@override
+Widget build(BuildContext context) {
+return const Center(
+child: Text('About information goes here'),
+);
+}
+}
+class User {
+final String firstName;
+final String email;
+User({required this.firstName, required this.email});
+factory User.fromJson(Map<String, dynamic> json) {
+return User(
+firstName: json['first_name'],
+email: json['email'],
+);
+}
+}
